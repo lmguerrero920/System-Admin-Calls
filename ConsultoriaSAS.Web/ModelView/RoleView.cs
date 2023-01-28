eaa85f8fc9bd2174d78d38c4dd6414f6e8cd3b03
Luis Miguel Guerrero Gutierrez <52765474+lmguerrero920@users.noreply.github.com>
2023-01-28 18:04:56 -0500

    Add files via upload

---
+++ ConsultoriaSAS.Web/ModelView/RoleView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsultoriaSAS.Web.ModelView
+
+{
+    public class RoleView
+    {
+        public string RoleId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ ConsultoriaSAS.Web/ModelView/UserView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ConsultoriaSAS.Web.ModelView
+
+{
+    public class UserView
+    {
+        public string UserId { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public  RoleView Role { get; set; }
+
+        public List<RoleView> Roles { get; set; }
+
+    }
+}
